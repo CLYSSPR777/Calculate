be417dae8ab83e0d06035eb59ebb56b1f7b6fac4
CLYSSPR777 <clys129@gmail.com>
2018-05-03 03:40:18 +0800

    Name it

---
+++ app/src/main/java/com/example/spr7/calculate/MainActivity_Calculate.java
@@ -0,0 +1,13 @@
+package com.example.spr7.calculate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Calculate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__calculate);
+    }
+}
